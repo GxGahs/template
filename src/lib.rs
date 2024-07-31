dd7072221b04a03b36d5e2a5193ba5836581159c
jackveithPITT <jvarsity2002@gmail.com>
2024-07-30 23:26:46 -0400

    notes

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,23 @@ extern crate alloc;
 mod title;
 
 pub fn main(mut agb: agb::Gba) -> ! {
+
     let (tiled, mut vram) = agb.display.video.tiled0();
-    vram.set;
+    
+    //what is tilesheet
+    vram.set_background_palettes(tile_sheet::PALETTES);
+
+    //create 2 backgrounds
+    //fill world_display (bg2) with tiles via set_tile
+    //
+    //mixer/sfx stuff
+    //
+    //commit world_display to vram
+    //show splash
+    //
+    //game loop
+    //
+
 }
 
 
